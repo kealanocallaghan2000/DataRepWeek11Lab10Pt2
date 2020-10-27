ca42b9ed593e195024e206ae9eb539c318842aac
KEALAN O CALLAGHAN <kealanocallaghan@gmail.com>
2020-10-27 12:40:46 +0000

    Lab 4 week 5 complete

--- src/components/create.js
+++ src/components/create.js
@@ -1,14 +1,90 @@
 // import react
-import React  from 'react';
+import React from 'react';
 
 
 // include export to import to app.js
-export class Create extends React.Component{
+export class Create extends React.Component {
 
-    render(){
-        return(
+    constructor() {
+        super();
+        //binding for the year, name and poster
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onChangeMovieName = this.onChangeMovieName.bind(this);
+        this.onChangeMovieYear = this.onChangeMovieYear.bind(this);
+        this.onChangeMoviePoster = this.onChangeMoviePoster.bind(this);
+
+        this.state = {
+            Title: '',
+            Year: '',
+            Poster: ''
+        }
+    }
+
+    //method for when values are changed
+    onChangeMovieName(e) {
+        this.setState({ Title: e.target.value })
+    }
+
+    onChangeMovieYear(e) {
+        this.setState({ Year: e.target.value })
+    }
+
+    onChangeMoviePoster(e) {
+        this.setState({ Poster: e.target.value })
+    }
+
+    //method from when text is submitted
+    onSubmit() {
+        alert('Movie added - Title: '
+            + this.state.Title + ' Year: '
+            + this.state.Year + ' Poster: '
+            + this.state.Poster);
+    }
+
+    render() {
+        return (
+            //UI
             <div>
                 <h1>This is the Create component</h1>
+                <form onSubmit={this.onSubmit}>
+                    {/* Name */}
+                    <div className="form/group">
+                        <label>Please add movie title</label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.Title}
+                            onChange={this.onChangeMovieName}>
+                        </input>
+                    </div>
+
+                    {/* Year */}
+                    <div className="form/group">
+                        <label>Please add movie year</label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.Year}
+                            onChange={this.onChangeMovieYear}>
+                        </input>
+                    </div>
+
+                    {/* Poster */}
+                    <div className="form/group">
+                        <label>Please add movie Poster</label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.Poster}
+                            onChange={this.onChangeMoviePoster}>
+                        </input>
+                    </div>
+
+
+                    {/* button for submtting movie data */}
+                    <div>
+                        <input type="submit"
+                            value="Add Movie">
+                        </input>
+                    </div>
+                </form>
             </div>
         );
     }
--- src/components/read.js
+++ src/components/read.js
@@ -1,6 +1,7 @@
 // import react
 import React  from 'react';
 import { Movies } from './movies';
+import axios from 'axios';
 
 
 // include export to import to app.js
@@ -9,30 +10,23 @@ export class Read extends React.Component{
     //object that handles the data for our components
     state = {
         //json data for movies
-        movies: [
-            {
-            "Title": "Avengers: Infinity War",
-            "Year": "2018",
-            "imdbID": "tt4154756",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-            },
-            {
-            "Title": "Captain America: Civil War",
-            "Year": "2016",
-            "imdbID": "tt3498820",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-            },
-            {
-            "Title": "Charlie Wilson's War",
-            "Year": "2007",
-            "imdbID": "tt0472062",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMTgwMDgwMDc4MF5BMl5BanBnXkFtZTYwOTU3MDM4._V1_SX300.jpg"
-            }
-            ]
+        movies: []
     };
+
+
+    //axios pulls json data from the link instead of hardcoding data
+    componentDidMount(){
+        axios.get('https://jsonblob.com/api/jsonblob/520c3b5e-0312-11eb-a6af-cbf00d776032')    
+        .then(response =>{
+            this.setState({movies: response.data.Search})
+        })
+        //catch for when an error occurs
+        .catch(
+            (error)=>{
+                console.log(error);
+            }    
+        );
+    }
 
     //visual piece of the component
     render(){
